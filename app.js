d67b6b1720022853a13c509791c9151d09c388d8
bohdanayabl <little.elf2004@gmail.com>
2024-04-21 20:17:30 +0200

    finalCommit

--- app.js
+++ app.js
@@ -6,22 +6,16 @@ const cars = require('./cars');
 const server = http.createServer((req, res) => {
     const carsData = cars.getCars();
     console.log(carsData);
-
     res.setHeader('Content-Type', 'text/html');
 
     res.write(htmlGenerator.getHTMLDocumentStart());
-
     res.write('<body>');
-
     carsData.forEach(car => {
         res.write(`<p>${cars.getCarInformation(car.id)}</p>`);
         res.write(`<p>${cars.getCarAge(car.id)}</p>`);
     });
-
     res.write('</body>');
-
     res.write(htmlGenerator.getHTMLDocumentEnd());
-
     res.end();
 });
 
